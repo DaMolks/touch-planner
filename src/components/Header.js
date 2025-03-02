44e965329c439a2a5f18fa0bff57c1e17ec70935
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-03-02 18:24:52 +0100

    Correction des problèmes d'encodage dans Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,23 @@
-import React from 'react';\nimport ReloadButton from './ReloadButton';\nimport './Header.css';\n\nconst Header = () => {\n  return (\n    <header className=\"header\">\n      <div className=\"container\">\n        <div className=\"header-content\">\n          <div className=\"header-left\">\n            <h1>Touch Planner</h1>\n            <p>Calculatrice de craft avancée pour Dofus Touch</p>\n          </div>\n          <div className=\"header-right\">\n            <ReloadButton />\n          </div>\n        </div>\n      </div>\n    </header>\n  );\n};\n\nexport default Header;\n
+import React from 'react';
+import ReloadButton from './ReloadButton';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-content">
+          <div className="header-left">
+            <h1>Touch Planner</h1>
+            <p>Calculatrice de craft avancee pour Dofus Touch</p>
+          </div>
+          <div className="header-right">
+            <ReloadButton />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
